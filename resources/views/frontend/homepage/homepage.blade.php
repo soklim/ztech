1fa31a7496ccd9457e3feb895620fb19c9dd764a
soklim <soklim.kheng@gmail.com>
2019-01-19 08:10:22 +0700

    Comming Soon

--- resources/views/frontend/homepage/homepage.blade.php
+++ resources/views/frontend/homepage/homepage.blade.php
@@ -2,6 +2,16 @@
 
     @section('content1')
 
-       <h1>Hello World</h1>
+        <div class="container-fluid bg-overlay">
+
+            <div class="container" style="text-align: center">
+                <a  href="https://www.facebook.com/Ztechnology168/" target="_blank" ><img src="/images/ztech.png"></a>
+                <h1 class="title-coming"><strong>COMING SOON</strong></h1>
+                {{--<a href="https://www.facebook.com/Ztechnology168/" target="_blank"><input type="button" class="btn btn-primary" value="CONTACT US"></a>--}}
+                <h4 style="padding-top: 30px">Contact Us: 012 845 007 | 093 609 007</h4>
+                {{--<img class="title-about" src="/images/brandheader/innisfree.png" width="200px">--}}
+
+            </div>
+        </div>
 
     @stop
